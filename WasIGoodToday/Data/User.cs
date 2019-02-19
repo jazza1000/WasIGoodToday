868824d3e1d52d5af4a5e497b597e5894852a1f3
Jamie Surman <jamiesurman@yahoo.com>
2019-02-19 13:05:20 +0000

    Added AccountController

---
+++ WasIGoodToday/Data/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; } //save a hash to the DB
+    }
+}
---
+++ WasIGoodToday/WasIGoodToday/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace WasIGoodToday.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly DataProvider m_DataProvider;
+        public AccountController(IConfiguration configuration)
+        {
+
+            string connection = configuration.GetConnectionString("wasIgoodConnection");
+            IMongoClient client = new MongoClient(connection);
+            m_DataProvider = new DataProvider(client);
+        }
+
+        [HttpPost]
+        public async Task<User> Post (string userName, string password)
+        {
+            //save user to database with hashed password
+        }
+
+
+        [HttpGet] //should this also be post??
+        public async Task<bool> Authorise (string userName, string password)
+        {
+
+        }
+    }
+}
